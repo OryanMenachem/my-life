14a92a1b515707fe08e161b96fd2eb744379cbd9
Oryan Menachem <om3160204@gmail.com>
2026-06-13 18:22:14 +0000

    File changes

--- src/components/BottomTabBar.jsx
+++ src/components/BottomTabBar.jsx
@@ -2,9 +2,9 @@ import { Link, useLocation } from "react-router-dom";
 
 const HomeIcon = ({ sw }) => (
   <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" strokeWidth={sw} strokeLinecap="round" strokeLinejoin="round">
-    <path d="M2 3h7a4 4 0 0 1 4 4v13a3 3 0 0 0-3-3H2z" />
-    <path d="M22 3h-7a4 4 0 0 0-4 4v13a3 3 0 0 1 3-3h7z" />
-    <polyline points="18 7 16.5 11 18 8.5" />
+    <path d="M12 6.4C10.2 5.2 7.6 4.6 4.5 4.8a1 1 0 0 0-.9 1V18a1 1 0 0 0 1.05 1c2.7-.16 5 .35 7.35 1.7" />
+    <path d="M12 6.4C13.8 5.2 16.4 4.6 19.5 4.8a1 1 0 0 1 .9 1V18a1 1 0 0 1-1.05 1c-2.7-.16-5 .35-7.35 1.7" />
+    <path d="M12 4.6V20.8l1.7-1.7 1.7 1.7V8.2" strokeWidth={sw * 1.25} />
   </svg>
 );
 
